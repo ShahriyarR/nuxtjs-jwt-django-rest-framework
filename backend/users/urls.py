270402bb6fccfa5a3bff622b802554e56545fa68
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2021-04-06 18:42:26 +0400

    Video 14 login endpoint backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, login
 
 urlpatterns = [
+    path('login', login),
     path('register', RegisterUserView.as_view(), name="register")
 ]
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,9 +3,43 @@ from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import RegisterUserSerializer
+from rest_framework.decorators import api_view
+from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+import requests
+
+
+
 
 
 class RegisterUserView(CreateAPIView):
     queryset = get_user_model().objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterUserSerializer
+    
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    
+    user = get_user_model().objects.filter(email=email).first()
+    if user is None:
+        raise exceptions.AuthenticationFailed('User not found!')
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed('Incorrect Password!')
+    
+    response = Response()
+    
+    token_endpoint = reverse(viewname='token_obtain_pair', request=request)
+    tokens = requests.post(token_endpoint, data=request.data).json()
+    
+    response.data = {
+        'access_token': tokens.get('access'),
+        'refresh_token': tokens.get('refresh'),
+        'email': user.email
+    }
+    
+    return response
+    
+    
